e18291f71e912d3a982bf80d152b146172b41a83
Joseph Wyman <jwymandev@gmail.com>
2020-08-29 14:55:38 -0400

    Add auth dal for login and signup

--- src/DAL/auth/authDal.js
+++ src/DAL/auth/authDal.js
@@ -1,17 +1,38 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const signUpUserModel = require("../../models/signUpUser");
+
+require("dotenv").config();
+
 module.exports = class AuthDal {
   constructor(db) {
     this.db = db;
   }
 
-  async findAllBooksByUser(userId) {
-    const allBooks = await this.db.collection("books").find({}).toArray();
+  async signUpUser(signUpData) {
+    const user = await signUpUserModel.validateAsync(signUpData);
+    const newUserToken = jwt.sign({ email: user.email }, process.env.HASHKEY); //
+    user.password = await bcrypt.hash(user.password, 8);
+    const newUserCreated = await this.db
+      .collection("users")
+      .insertOne({ token: newUserToken, ...user });
 
-    const allBooksReadByUser = await this.db
-      .collection("books-read")
-      .find({ userId: userId })
-      .toArray();
-    const arrayOfBookIdsRead = allBooksReadByUser.map((bookRead) => {
-      return bookRead.bookId;
-    });
+    return newUserToken;
+  }
+
+  async loginUser(loginData) {
+    const user = await signUpUserModel.validateAsync(loginData);
+    const dbUser = await this.db
+      .collection("users")
+      .findOne({ username: user.username });
+    const isPasswordMatch = await bcrypt.compare(
+      user.password,
+      dbUser.password
+    );
+    if (isPasswordMatch) {
+      return dbUser.token;
+    } else {
+      throw new ValidationError("password and username do not match");
+    }
   }
 };
--- src/DAL/dalDB.js
+++ src/DAL/dalDB.js
@@ -1,11 +1,8 @@
-
-  module.exports = class DalDb{
-
-    constructor(db,  BooksDal, NotesDal){ 
-        this.db = db
-        this.notesDal = new NotesDal(db)
-        this.booksDal = new BooksDal(db) 
-    }
-
-
-}
+module.exports = class DalDb {
+  constructor(db, BooksDal, NotesDal, AuthDal) {
+    this.db = db;
+    this.notesDal = new NotesDal(db);
+    this.booksDal = new BooksDal(db);
+    this.authDal = new AuthDal(db);
+  }
+};
